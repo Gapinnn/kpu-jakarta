83553c6298b8d4a93fba245669193eeb291a1a66
sultanhadip <222112384@stis.ac.id>
2024-06-09 07:14:43 +0700

    halaman sejarah

---
+++ src/pages/Sejarah.jsx
@@ -0,0 +1,11 @@
+import SejarahComponent from "../components/SejarahComponent";
+
+export default function Sejarah() {
+  return (
+    <>
+      <div className="flex flex-col gap-0">
+        <SejarahComponent />
+      </div>
+    </>
+  );
+}
