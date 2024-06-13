8112688979d2d57315380b552d37f74bd02d7eb4
Gavin Atha Wisesa <222112069@stis.ac.id>
2024-06-13 12:40:52 +0700

    responsif header & footer

---
+++ src/components/Icon/Hamburger.jsx
@@ -0,0 +1,13 @@
+export default function Hamburger({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      className={className}
+    >
+      <path fill="currentColor" d="M3 6h18v2H3zm0 5h18v2H3zm0 5h18v2H3z" />
+    </svg>
+  );
+}
