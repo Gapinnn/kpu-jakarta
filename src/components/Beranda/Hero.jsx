a052ff7c5f74ee3fd3f01004c9c96284d29ce6c9
Gavin Atha Wisesa <222112069@stis.ac.id>
2024-06-14 23:49:34 +0700

    fix responsif kpu dki jakarta

--- src/components/Beranda/Hero.jsx
+++ src/components/Beranda/Hero.jsx
@@ -1,4 +1,3 @@
-import SearchBar from "./SearchBar";
 import DataSlider from "./DataSlider";
 import Typed from "typed.js";
 import { useEffect, useRef } from "react";
@@ -25,18 +24,35 @@ export default function Hero() {
   }, []);
 
   return (
-    <div className="relative w-full h-fit m-0 p-0">
-      <img src="/images/hero.png" alt="Hero KPU" className="w-full" />
-      <div className="absolute top-44 w-full flex justify-center">
-        <div className="w-full flex flex-col gap-14">
+    <div className="relative flex w-full h-fit m-0 p-0">
+      {/* Hero Hp */}
+      <img
+        src="/images/hero.png"
+        alt="Hero KPU"
+        className="flex md:hidden h-[756px] w-full object-cover object-center m-0 p-0"
+      />
+      {/* Hero Hp */}
+      <img
+        src="/images/hero.png"
+        alt="Hero KPU"
+        className="hidden md:flex lg:hidden h-[900px] w-full object-cover object-center m-0 p-0"
+      />
+      {/* Hero Laptop */}
+      <img
+        src="/images/hero.png"
+        alt="Hero KPU"
+        className="hidden lg:flex w-full"
+      />
+      <div className="absolute top-36 md:top-44 lg:top-44 w-full flex justify-center">
+        <div className="w-full flex flex-col gap-6 md:gap-10 lg:gap-14">
           <div className="flex flex-col w-fit self-center gap-2">
-            <div className="flex flex-col gap-2">
-              <h1 className="text-stone-100 text-center mx-auto font-extrabold text-6xl">
-                Pemilihan Umum DKI Jakarta 2024
+            <div className="flex flex-col gap-2 md:gap-3 lg:gap-2">
+              <h1 className="text-stone-100 text-center mx-auto font-extrabold text-4xl md:text-5xl lg:text-6xl">
+                Pemilihan Umum <br className="lg:hidden" /> DKI Jakarta 2024
               </h1>
               <h1
                 ref={el}
-                className="text-stone-100 italic text-center mx-auto font-extrabold text-4xl"
+                className="text-stone-100 italic text-center mx-auto font-extrabold text-2xl md:text-3xl lg:text-4xl px-2 md:px-16 lg:px-0"
               ></h1>
             </div>
             {/* <SearchBar /> */}
--- src/components/Beranda/InfoSlider.jsx
+++ src/components/Beranda/InfoSlider.jsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
 import Slider from "react-slick";
 import { dataInfo } from "../../contents/hero";
 import NextArrow from "../Icon/NextArrow";
 import PrevArrow from "../Icon/PrevArrow";
 
 export default function InfoSlider() {
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+  const [loadedCount, setLoadedCount] = useState(0);
+
+  useEffect(() => {
+    if (loadedCount === dataInfo.length) {
+      setImagesLoaded(true);
+    }
+  }, [loadedCount]);
+
+  const handleImageLoad = () => {
+    setLoadedCount((prevCount) => prevCount + 1);
+  };
+
   function PrevArroww(props) {
     const { onClick } = props;
     return (
@@ -11,7 +25,7 @@ export default function InfoSlider() {
         className="w-fit rounded-full flex justify-center items-center"
         onClick={onClick}
       >
-        <PrevArrow className={"w-16 h-16"} />
+        <PrevArrow className="w-12 h-12 md:w-14 md:h-14 lg:w-16 lg:h-16" />
       </div>
     );
   }
@@ -23,7 +37,7 @@ export default function InfoSlider() {
         className="rounded-full flex justify-center items-center"
         onClick={onClick}
       >
-        <NextArrow className={"w-16 h-16"} />
+        <NextArrow className="w-12 h-12 md:w-14 md:h-14 lg:w-16 lg:h-16" />
       </div>
     );
   }
@@ -38,9 +52,7 @@ export default function InfoSlider() {
     pauseOnHover: true,
     nextArrow: <NextArroww />,
     prevArrow: <PrevArroww />,
-    appendDots: (
-      dots // Atur List DOT
-    ) => (
+    appendDots: (dots) => (
       <div className="flex justify-center items-center w-full rounded-lg">
         <ul className="flex gap-2 justify-center"> {dots} </ul>
       </div>
@@ -48,21 +60,53 @@ export default function InfoSlider() {
     customPaging: () => (
       <div className="w-5 h-5 rounded-full bg-maroon-light bg-opacity-60 hover:scale-110 hover:bg-opacity-100"></div>
     ),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
+
   return (
     <div id="infoSlider">
-      <div className="w-3/4 h-fit slider-container mx-auto z-20 mt-8 pb-12">
-        <Slider {...settings} className="flex justify-center items-center">
-          {dataInfo.map((data, index) => (
-            <div key={index} className="p-2 z-20">
+      <div className="w-full lg:w-3/4 lg:h-fit lg:mx-auto mt-4 md:mt-6 lg:mt-8 pb-8 md:pb-10 lg:pb-12">
+        {imagesLoaded ? (
+          <Slider {...settings} className="flex justify-center items-center">
+            {dataInfo.map((data, index) => (
+              <div key={index} className="p-0 md:p-2 z-20">
+                <img
+                  src={`/images/${data.file}`}
+                  alt={data.keterangan}
+                  className="w-full object-cover rounded-lg m-0 p-0"
+                />
+              </div>
+            ))}
+          </Slider>
+        ) : (
+          <div className="flex justify-center items-center">
+            Loading...
+            {dataInfo.map((data, index) => (
               <img
+                key={index}
                 src={`/images/${data.file}`}
                 alt={data.keterangan}
-                className="w-full object-cover rounded-lg"
+                className="hidden"
+                onLoad={handleImageLoad}
               />
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
