b10d7e0f8192fa894b8be0a91aedea3aab077520
Gavin Atha Wisesa <222112069@stis.ac.id>
2024-06-21 11:06:51 +0700

    bug localhost

--- src/hooks/getLanguage.js
+++ src/hooks/getLanguage.js
@@ -1,5 +1,7 @@
 export default function getLanguage() {
   // Ekstrak ID/EN dari URL
-  const path = window.location.href.split("5173")[1].split("/");
+  const path = window.location.href
+    .split("kpu-jakarta.vercel.app")[1]
+    .split("/");
   return path[1];
 }
